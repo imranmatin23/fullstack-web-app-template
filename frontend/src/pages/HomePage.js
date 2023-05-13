3e0a287b6c6ed14278db26fcd3efa3fb5879a79b
Imran Matin <imranmatin23@gmail.com>
2023-05-13 16:02:53 -0700

    Setup basic frontend

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+
+function HomePage() {
+  axios
+    .get("/api/room")
+    .then((response) =>
+      console.log(`Number of Rooms in the database: ${response.data.length}`)
+    )
+    .catch((error) => {
+      console.error("There was an error!", error);
+    });
+
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Fullstack Web App Template</h2>
+      </div>
+      <p>
+        This is the home page! Open the Javascript console in your browser to
+        see the number of rooms in the database.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
