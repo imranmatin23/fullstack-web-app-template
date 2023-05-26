c64d19ce375f382f3f4c8751afc3181273ba5951
Imran Matin <imranmatin23@gmail.com>
2023-05-26 12:53:24 -0700

    Add .dockerignore and support LOCAL flag to use Gunicorn or Django development server

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # Collect static assets
-python /app/manage.py collectstatic --noinput
+echo "Collecting the static assets..."
+python manage.py collectstatic --noinput
 
-# Run the Django server using Gunicorn
-gunicorn --bind 0.0.0.0:80 --workers 1 --timeout 60 --log-level debug backend.wsgi
-
-# Run the Django server using Django
-# python manage.py runserver 0.0.0.0:80
+if [ "$LOCAL" = "True" ]; then
+    echo "The LOCAL flag is $LOCAL, running Django development server..."
+    # Run the Django server using Django
+    python manage.py runserver 0.0.0.0:80
+else
+    echo "The LOCAL flag is $LOCAL, running Django Gunicorn server..."
+    # Run the Django server using Gunicorn
+    gunicorn --bind 0.0.0.0:80 --workers 1 --timeout 60 --log-level debug backend.wsgi
+fi
